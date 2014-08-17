b78a99f127ef6a7b27be2ef1fabc2ed74f2b6c3a
Bert Pattyn <bert@dextrose.be>
2014-08-17 21:56:27 +0200

    add function to copy my company pubkey to the pasteboard

--- system/keys.zsh
+++ system/keys.zsh
@@ -1,2 +1,3 @@
-# Pipe my public key to my clipboard.
-alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
+# Pipe my public keys to my clipboard.
+alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Personal public key copied to pasteboard'"
+alias pubkeywijs="more ~/.ssh/wijs_rsa.pub | pbcopy | echo '=> Wijs public key copied to pasteboard'"
