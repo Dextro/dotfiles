20d836eed7c2f75cf4b9fb60fc13302e624ea976
Bert Pattyn <bert@dextrose.be>
2014-08-17 21:59:53 +0200

    change some of the default zsh options

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,4 +1,12 @@
 alias reload!='. ~/.zshrc'
 
+# push and pop directories on directory stack
+alias pu='pushd'
+alias po='popd'
+
+# super user
+alias _='sudo'
+alias please='sudo'
+
 # disable autocorrect and run as admin
 alias mtr="nocorrect sudo mtr"
