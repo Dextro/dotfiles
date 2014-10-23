f6bbca47d4a29e63fe78db294428c7bd9fceea56
Bert Pattyn <bert@dextrose.be>
2014-10-23 22:08:00 +0200

    change some git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -6,14 +6,17 @@ then
   alias git=$hub_path
 fi
 
+# main alias
+alias g='git'
+
 # The rest of my fun git aliases
-alias gl='git pull --prune'
+alias gpull='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gp='git push origin HEAD'
+alias gpush='git push origin HEAD'
 alias gd='git diff'
-alias gc='git commit'
-alias gca='git commit -a'
-alias gco='git checkout'
+alias gco='git commit'
+alias gcoa='git commit -a'
+alias gch='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
