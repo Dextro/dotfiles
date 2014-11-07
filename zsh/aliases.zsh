61f10c53a4a41d56398c828b7be8169704507182
Bert Pattyn <bert@dextrose.be>
2014-11-07 23:12:07 +0100

    remove unused zsh aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,12 +1,4 @@
 alias reload!='. ~/.zshrc'
 
-# push and pop directories on directory stack
-alias pu='pushd'
-alias po='popd'
-
-# super user
-alias _='sudo'
-alias please='sudo'
-
 # disable autocorrect and run as admin
 alias mtr="nocorrect sudo mtr"
