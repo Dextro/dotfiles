7d409e4ef7795e41760a216e83d0c3db162207f1
Bert Pattyn <bert@dextrose.be>
2015-05-24 15:50:25 +0200

    add function to generate random passwords

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,3 +3,11 @@ alias upgrade!='dot'
 
 # disable autocorrect and run as admin
 alias mtr="nocorrect sudo mtr"
+
+# generate password
+# optional variable is the length of the password (otherwise 26)
+genpwd() {
+  PW=$(openssl rand -base64 32 | head -c${1-26});
+  echo 'new password: '$PW;
+  echo $PW | pbcopy;
+}
