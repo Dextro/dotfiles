0669db2e69ee97738b1e6def1869a13f09b779f5
Bert Pattyn <bert@dextrose.be>
2016-06-29 11:31:40 +0200

    update the zsh prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,37 +1,6 @@
-autoload colors && colors
-# cheers, @ehrenmurdick
-# http://github.com/ehrenmurdick/config/blob/master/zsh/prompt.zsh
+# load colors - https://wiki.archlinux.org/index.php/zsh#Colors
+autoload -Uz colors && colors
 
-if (( $+commands[git] ))
-then
-  git="$commands[git]"
-else
-  git="/usr/bin/git"
-fi
-
-git_branch() {
-  echo $($git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
-}
-
-git_dirty() {
-  if $(! $git status -s &> /dev/null)
-  then
-    echo ""
-  else
-    if [[ $($git status --porcelain) == "" ]]
-    then
-      echo "on %{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
-    else
-      echo "on %{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}"
-    fi
-  fi
-}
-
-git_prompt_info () {
- ref=$($git symbolic-ref HEAD 2>/dev/null) || return
-# echo "(%{\e[0;33m%}${ref#refs/heads/}%{\e[0m%})"
- echo "${ref#refs/heads/}"
-}
 
 unpushed () {
   $git cherry -v @{upstream} 2>/dev/null
@@ -58,6 +27,26 @@ ruby_version() {
   fi
 }
 
+python_version() {
+  if (( $+commands[python] ))
+  then
+    echo "$(python --version 2>&1 | awk '{print $2}')"
+  fi
+}
+
+python_virtualenv() {
+  if [ -n "$VIRTUAL_ENV" ]; then
+    if [ -f "$VIRTUAL_ENV/__name__" ]; then
+      local name=`cat $VIRTUAL_ENV/__name__`
+    elif [ `basename $VIRTUAL_ENV` = "__" ]; then
+      local name=$(basename $(dirname $VIRTUAL_ENV))
+    else
+      local name=$(basename $VIRTUAL_ENV)
+    fi
+    echo " $name"
+  fi
+}
+
 rb_prompt() {
   if ! [[ -z "$(ruby_version)" ]]
   then
@@ -67,16 +56,51 @@ rb_prompt() {
   fi
 }
 
-directory_name() {
-  echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
+# dump the current git branch
+git_current_branch() {
+  echo $($git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
 }
 
-export PROMPT=$'\n$(rb_prompt)in $(directory_name) $(git_dirty)$(need_push)\n› '
-set_prompt () {
-  export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
+# dump the color to use based on the state of the git repo
+git_dirty() {
+  if $(! $git status -s &> /dev/null)
+  then
+    echo ""
+  else
+    if [[ $($git status --porcelain) == "" ]]
+    then
+      echo "$fg[green]"
+    else
+      echo "$fg[red]"
+    fi
+  fi
+}
+
+# dump git prompt information
+function git_prompt_info() {
+  ref=$(git symbolic-ref HEAD 2> /dev/null) || return
+  echo "$(git_dirty)$(git_current_branch)$reset_color"
+}
+
+# Get the current working directory
+get_pwd() {
+  echo "${PWD/$HOME/~}"
 }
 
 precmd() {
   title "zsh" "%m" "%55<...<%~"
-  set_prompt
+  # export PROMPT=$' in $(directory_name) $(git_dirty)$(need_push)\n› '
+  local LEFT
+  local RIGHT
+  local RIGHTWIDTH
+
+  LEFT="$fg[cyan]%m: $fg[yellow]$(get_pwd)"
+  RIGHT="$(git_prompt_info)"
+  RIGHTWIDTH=$(($COLUMNS-${#LEFT}+${#RIGHT}))
+
+  export PROMPT="
+$LEFT${(l:$RIGHTWIDTH:::)RIGHT}
+$reset_color→ "
+  # export RPROMPT="%{$fg_bold[cyan]%} R $(rb_prompt)- P $(python_version)$(python_virtualenv)%{$reset_color%}"
+  export RPROMPT=""
 }
