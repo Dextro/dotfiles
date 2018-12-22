2b6fcdf7e596a6fa5c0e4854eb0abd65622387d9
Bert Pattyn <bert@dextrose.be>
2018-12-22 14:53:29 +0100

    change dotfile root paths

--- docker/completion.zsh
+++ docker/completion.zsh
@@ -1,16 +1,16 @@
 # run script/install first
 
-docker_completion_file="$(pwd)/cache/_docker_completion"
-docker_compose_completion_file="$(pwd)/cache/_docker_compose_completion"
+docker_completion_file="$ZSH/cache/_docker_completion"
+docker_compose_completion_file="$ZSH/cache/_docker_compose_completion"
 
 if test -f $docker_completion_file
 then
   source $docker_completion_file
-  echo "-> docker completion loaded"
+  echo "→ docker completion loaded"
 fi
 
 if test -f $docker_compose_completion_file
 then
   source $docker_compose_completion_file
-  echo "-> docker compose completion loaded"
+  echo "→ docker compose completion loaded"
 fi
--- docker/install.sh
+++ docker/install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-curl -L --silent https://raw.githubusercontent.com/docker/cli/master/contrib/completion/zsh/_docker --output "$(pwd)/cache/_docker_completion"
-curl -L --silent https://raw.githubusercontent.com/docker/compose/master/contrib/completion/zsh/_docker-compose --output "$(pwd)/cache/_docker_compose_completion"
+curl -L --silent https://raw.githubusercontent.com/docker/cli/master/contrib/completion/zsh/_docker --output "$ZSH/cache/_docker_completion"
+curl -L --silent https://raw.githubusercontent.com/docker/compose/master/contrib/completion/zsh/_docker-compose --output "$ZSH/cache/_docker_compose_completion"
