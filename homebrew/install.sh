d470858a10a6027f1079fb793adc778a3abd3cd6
Bert Pattyn <bert.pattyn@wijs.be>
2019-12-15 21:49:35 +0100

    run homebrew unattended

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -7,7 +7,7 @@
 if test ! $(which brew)
 then
   echo "→→→ Installing Homebrew..."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" > /tmp/homebrew-install.log
+  CI=1 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 # Make sure we’re using the latest Homebrew
