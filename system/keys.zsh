90498efab17b71a4bc1604a6ee428a952ddf079e
Bert Pattyn <bert.pattyn@wijs.be>
2020-01-08 14:01:51 +0100

    change pubkeywijs command

--- system/keys.zsh
+++ system/keys.zsh
@@ -1,3 +1,3 @@
 # Pipe my public keys to my clipboard.
 alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Personal public key copied to pasteboard'"
-alias pubkeywijs="more ~/.ssh/wijs_rsa.pub | pbcopy | echo '=> Wijs public key copied to pasteboard'"
+alias pubkeydg="more ~/.ssh/dg_rsa.pub | pbcopy | echo '=> Duke & Grace public key copied to pasteboard'"
