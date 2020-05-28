12919fca42a46bf322e6201e9608e1335c249c08
Bert Pattyn <bert.pattyn@wijs.be>
2020-05-28 14:32:14 +0200

    add pyenv vars

--- python/python.zsh
+++ python/python.zsh
@@ -1,2 +1,6 @@
 # pip should only run if there is a virtualenv currently activated
 export PIP_REQUIRE_VIRTUALENV=false
+
+# add pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
