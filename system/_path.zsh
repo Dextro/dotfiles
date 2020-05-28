8186049acd0799cddcc35870846375a484033b40
Bert Pattyn <bert.pattyn@wijs.be>
2020-05-28 14:33:04 +0200

    cleanup PATH variable

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,6 +1,3 @@
-# add homebrew paths to PATH
-export PATH="/usr/local/sbin:$PATH"
-
 # reset PATH variable
 export PATH=$(getconf PATH)
 
@@ -9,16 +6,6 @@ export PATH="/usr/local/bin:/usr/local/sbin:$PATH"
 
 # add dotfiles bin folder to PATH
 export PATH="$DOTFILES/bin:$PATH"
-
-# add composer stuff to PATH
-export PATH="$HOME/.composer/vendor/bin:$PATH"
-
-# add node stuff to PATH
-export PATH="/usr/local/opt/node@8/bin:$PATH"
-
-# php
-export PATH="/usr/local/opt/php@7.2/bin:$PATH"
-export PATH="/usr/local/opt/php@7.2/sbin:$PATH"
 
 # add personal bin to PATH
 export PATH="$HOME/bin:$PATH"
