78605410a7cb73a0195673d1ae7558c49a1ad24c
Bert Pattyn <bert.pattyn@wijs.be>
2020-05-28 14:22:38 +0200

    make git aliases shorter

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -10,29 +10,29 @@ fi
 alias g='git'
 
 # The rest of my fun git aliases
-alias gitpull='git pull --prune'
-alias gitlog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gitpush='git push origin HEAD'
-alias gitd='git diff'
-alias gitco='git commit'
-alias gitcoa='git commit -a'
-alias gitch='git checkout'
-alias gitcb='git copy-branch-name'
-alias gitb='git branch'
-alias gits='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
-alias gita='git add'
-alias gitap='git add -p'
-alias gitaa='git add -A' # stages all
-alias gitamfi="git-amend"
-alias gitcr="git-credit"
-alias gitdellm="git-delete-local-merged"
-alias gitnuke="git-nuke"
-alias gitrank="git-rank-contributors"
-alias gittr="git-track"
-alias gittra="git-track-all"
-alias gitundo="git-undo"
-alias gitunp="git-unpushed"
-alias gitunpstat="git-unpushed-stat"
-alias gitup="git-up"
-alias gitreup="git-reup"
-alias gitwtf="git-wtf"
+alias gpull='git pull --prune'
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias gpush='git push origin HEAD'
+alias gd='git diff'
+alias gco='git commit'
+alias gcoa='git commit -a'
+alias gch='git checkout'
+alias gcb='git copy-branch-name'
+alias gb='git branch'
+alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias ga='git add'
+alias gap='git add -p'
+alias gaa='git add -A' # stages all
+alias gamfi="git-amend"
+alias gcr="git-credit"
+alias gdellm="git-delete-local-merged"
+alias gnuke="git-nuke"
+alias grank="git-rank-contributors"
+alias gtr="git-track"
+alias gtra="git-track-all"
+alias gundo="git-undo"
+alias gunp="git-unpushed"
+alias gunpstat="git-unpushed-stat"
+alias gup="git-up"
+alias greup="git-reup"
+alias gwtf="git-wtf"
